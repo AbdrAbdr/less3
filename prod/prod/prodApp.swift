91ead803e5d072dc38c7dc67b3daf347ae7af7e0
abdr <unilife.ch@gmail.com>
2024-02-12 12:04:48 +0500

    sv1

---
+++ prod/prod/prodApp.swift
@@ -0,0 +1,17 @@
+//
+//  prodApp.swift
+//  prod
+//
+//  Created by  abdr on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct prodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
